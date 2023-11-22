a6bcf7b3f68ca7a87b5714df44776ae35eda0f3f
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2023-11-22 13:10:57 +0100

    start web video server local

--- start_local.sh
+++ start_local.sh
@@ -5,4 +5,5 @@ fi
 cd "$(dirname "$0")" || exit
 source install/setup.bash
 export ROS_DOMAIN_ID=1
+ros2 run web_video_server web_video_server || true
 ros2 launch robotino_ros2 robotino_local.launch.py
